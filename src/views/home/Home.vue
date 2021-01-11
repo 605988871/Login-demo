17e07792a254ba6dc9bd49d415100ce3916b8f04
605988871 <605988871@qq.com>
2021-01-11 17:33:00 +0800

    播放器拆分成组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -113,6 +113,7 @@
       "
     ></div>
     <div class="toTop" @click="toTop"></div>
+    <aplayer></aplayer>
   </div>
 </template>
 
@@ -121,11 +122,14 @@
 import axios from 'axios'
 import { RENDERER } from '../../utils/fish'
 import { L2Dwidget } from 'live2d-widget'
-import APlayer from 'aplayer'
 import 'aplayer/dist/APlayer.min.css'
+import Aplayer from './components/aplayer.vue'
 
 export default {
   name: 'Home',
+  components: {
+    Aplayer
+  },
   data() {
     return {
       isReloadAlive: true,
@@ -156,7 +160,7 @@ export default {
   async mounted() {
     this.loading1 = true
     this.loading2 = true
-    await this.createAplayer()
+
     if (typeof Storage !== 'undefined') {
       // Store
       if (localStorage.getItem('jinrishiciToken') == null) {
@@ -254,79 +258,7 @@ export default {
       this.gushi.author = res.data.data.origin.author
       this.gushi.title = res.data.data.origin.title
     },
-    async getSongUrl(keywords) {
-      const resSongId = await axios.get('/music/cloudsearch', {
-        params: {
-          keywords: keywords
-        }
-      })
-      let songId = resSongId.data.result.songs[0].id
-      // const resSongUrl = await axios.get('/music/song/url', {
-      //   params: {
-      //     id: songId
-      //   }
-      // })
-      // return resSongUrl.data.data[0].url
-      const url =
-        'https://music.163.com/song/media/outer/url?id=' + songId + '.mp3'
-      return url
-    },
-    async getSongLyric(keywords) {
-      const resSongId = await axios.get('/music/cloudsearch', {
-        params: {
-          keywords: keywords
-        }
-      })
-      let songId = resSongId.data.result.songs[0].id
-      const resSongUrl = await axios.get('/music/lyric', {
-        params: {
-          id: songId
-        }
-      })
-      return resSongUrl.data.lrc.lyric.toString()
-    },
-    async createAplayer() {
-      const ap = new APlayer({
-        container: document.getElementById('aplayer'),
-        fixed: true,
-        lrcType: 1,
-        // mini: false,
-        audio: [
-          {
-            name: '光るなら',
-            artist: 'Goose House',
-            url: await this.getSongUrl('光るなら'),
-            lrc: await this.getSongLyric('光るなら'),
-            cover:
-              'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201504%2F25%2F20150425H2120_PtLQe.thumb.700_0.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612594686&t=daace713fef7674cab1fef143c148642'
-          },
-          {
-            name: ' トリカゴ',
-            artist: 'XX:me',
-            url: await this.getSongUrl('トリカゴ'),
-            lrc: await this.getSongLyric('トリカゴ'),
-            cover:
-              'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2684230618,3665831027&fm=26&gp=0.jpg'
-          },
-          {
-            name: '夜に駆ける',
-            artist: 'YOASOBI',
-            url: await this.getSongUrl('夜に駆ける'),
-            lrc: await this.getSongLyric('夜に駆ける'),
-            cover:
-              'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ftx-free-imgs.acfun.cn%2Fo_1eedd5k9l16gi1e501jtss9mbfo0.jpeg%3Fimageslim&refer=http%3A%2F%2Ftx-free-imgs.acfun.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612666822&t=c5ff4ee5ad6d73ac4079d2c49e67a127'
-          },
-          {
-            name: 'オレンジ',
-            artist: '7!!',
-            url: await this.getSongUrl('オレンジ'),
-            lrc: await this.getSongLyric('オレンジ'),
-            cover:
-              'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201504%2F25%2F20150425H2120_PtLQe.thumb.700_0.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612594686&t=daace713fef7674cab1fef143c148642'
-          }
-        ]
-      })
-    },
+
     loginOut() {
       this.$loginOut()
     },
